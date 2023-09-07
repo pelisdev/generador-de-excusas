6b616b564f45eefaae86266f89e8961836fa9b65
Alvaro <curielyseguro@gmail.com>
2023-09-08 01:42:34 +0200

    hecho

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const ExcusaRandom = () => {
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    const excusaAleatoria = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+    document.getElementById("excusa").textContent = excusaAleatoria;
+  };
+
+  const botonExcusa = document.getElementById("generarExcusa");
+  botonExcusa.addEventListener("click", ExcusaRandom);
+
+  ExcusaRandom();
   console.log("Hello Rigo from the console!");
 };
